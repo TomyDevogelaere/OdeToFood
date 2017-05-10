bdf27861df027b693defa6b3682b482d662e5379
devtom <tomy.devogelaere@telenet.be>
2017-05-10 08:27:44 +0200

    Add Migrations and db support works

--- OdeToFood2017/Entities/OdeToFoodDbContext.cs
+++ OdeToFood2017/Entities/OdeToFoodDbContext.cs
@@ -8,6 +8,7 @@ namespace OdeToFood2017.Entities
 {
     public class OdeToFoodDbContext : DbContext
     {
+        public OdeToFoodDbContext(DbContextOptions options) : base(options) { }
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
---
+++ OdeToFood2017/Migrations/20170510054936_v1.Designer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using OdeToFood2017.Entities;
+
+namespace OdeToFood2017.Migrations
+{
+    [DbContext(typeof(OdeToFoodDbContext))]
+    [Migration("20170510054936_v1")]
+    partial class v1
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("OdeToFood2017.Entities.Restaurant", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Cuisine");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(80);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Restaurants");
+                });
+        }
+    }
+}
